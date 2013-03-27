684d0524d826950b973e01517710c588d400c384
Dennis Cessan <dennis.cessan@gmail.com>
2013-03-27 22:24:55 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# Gems used for the application
+gem 'jquery-rails'
+
+
+# Gems used only in development
+group :development do
+	gem 'sqlite3'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+# Gems used only for production
+group :production do
+	gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DishMe::Application.config.secret_token = '86db90c98036b867b9ff6d8ba36961221204699eac242a2aabc6ad2c866059cd7fe9f1c70063e24c206c4bf7fecb4037936edf16e6122ee33aa6dda9ee92d1c9'
