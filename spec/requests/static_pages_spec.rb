6669f5dc7baf5b5edd1aaa7a3e219e84c3807811
Dennis Cessan <dennis.cessan@gmail.com>
2013-03-28 00:01:23 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+	
+	describe "Static pages" do
+  	
+  	describe "Home page" do
+    	it "should have the content 'Welcome to dish.me'" do
+      	visit '/static_pages/home'
+      	page.should have_content('Welcome to dish.me')
+    	end 
+  	end
+  	
+  	describe "Help page" do
+  		it "should have the content 'Help'" do
+  			visit '/static_pages/help'
+  				page.should have_content('Help')
+				end
+			end  
+
+		describe "About page" do
+			it "should have the content 'About dish.me'" do
+				visit '/static_pages/about'
+					page.should have_content('About dish.me')
+				end	  	
+			end
+
+	end
---
+++ spec/requests/titles_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "Titles" do
+
+  describe "Home page" do
+	  it "should have the right title" do
+	  	visit '/static_pages/home'
+	  		page.should have_selector('title', :text => 'Dish.me')
+		end
+	end
+	
+	describe "Help page" do
+		it "should have the right title" do
+			visit '/static_pages/help'
+				page.should have_selector('title', :text => 'Dish.me | Help')
+		end
+	end
+	
+	describe "About page" do
+		it "should have the right title" do
+			visit '/static_pages/about'
+				page.should have_selector('title', :text => 'Dish.me | About')
+		end
+	end															
+end
